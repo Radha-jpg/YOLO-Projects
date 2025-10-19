62c447d6cd3189ebd784d58747007b8d9a9a368d
Radha Kumari Ray <64279481+Radha-jpg@users.noreply.github.com>
2025-10-19 13:53:17 +0100

    Add files via upload

---
+++ yolo_race.py
@@ -0,0 +1,16 @@
+import cv2
+from ultralytics import YOLO
+model = YOLO("yolov8n.pt")
+cap = cv2.VideoCapture(0)
+while True:
+    ret,frame = cap.read()
+    if not ret:
+        break
+    results = model(frame) #run yolo on each frame
+    annotated_frame = results[0].plot() #draw boxes
+    cv2.imshow("YOLO v8 Detection",annotated_frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
